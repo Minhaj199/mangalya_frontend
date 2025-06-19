c75611b56522a6c47ff8e715297d4af37a715b7c
minhaj199 <minhajpuzhakkal@gmail.com>
2025-06-19 10:20:45 +0530

    formated

--- src/admin/login/Login.tsx
+++ src/admin/login/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
 
-import { request } from '../../../index' 
+import { request } from '../../utils/axiosUtils' 
 
 import { LoginValidatorAdmin } from "../../validators/loginValidatorForAdmin";
 import { useNavigate } from "react-router-dom";
