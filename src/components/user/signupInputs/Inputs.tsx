ab924df82a95e0d7863347563750bcf98ea4ccd2
minhaj199 <minhajpuzhakkal@gmail.com>
2025-08-27 17:44:43 +0530

    signup select changed

--- src/components/user/signupInputs/Inputs.tsx
+++ src/components/user/signupInputs/Inputs.tsx
@@ -3,6 +3,7 @@
 import {  IInputsProbs } from '@/types/typesAndInterfaces' 
 import { Validator_ } from '../../../validators/liveValidator' 
 import { capitaliser } from '../../../utils/firstLetterCapitaliser'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 
 
@@ -22,6 +23,7 @@ export const Inputs:React.FC<IInputsProbs> = ({inputFields,setCredentialData,set
     Validator_(setWarnning,field,value)
    
   }
+
   
   return (<>
     {inputFields.map((fields,index)=>{
@@ -29,17 +31,28 @@ export const Inputs:React.FC<IInputsProbs> = ({inputFields,setCredentialData,set
         <div className=' sm:w-4/5 flex flex-col ' key={index}>
           <label htmlFor={fields.linkingName} className="text-white text-xs sm:text-base block   w-full">{fields.inputName}</label>
       {(fields.inputType!=='dropDown')?
-      <input  type={fields.inputType} onChange={(t)=>handleChange(t,fields.inputName)} value={CredentailData[fields.inputName]} id={fields.linkingName} className="w-full font-serif   border  border-theme-blue  outline-none h-9 block pl-3" />:
-      <select className='w-full    border  border-theme-blue  outline-none h-9  block pl-3' value={CredentailData[fields.inputName]} onChange={(t)=>setCredentialData(prev=>({...prev,[fields.inputName]:t.target.value}))} id={fields.linkingName}>
-        <option value=''>SELECT</option>
-        {fields.option?.map((el,index)=>{
-          return(
-            <>
-            <option value={el} key={index}>{el}</option>
-            </>
-          )
-        })}
-      </select>
+      <input  type={fields.inputType} onChange={(t)=>handleChange(t,fields.inputName)} value={CredentailData[fields.inputName]} id={fields.linkingName} className="w-full font-serif rounded-md   border  border-theme-blue  outline-none h-9 block pl-3" />:
+      // <select className='w-full    border  border-theme-blue  outline-none h-9  block pl-3' value={CredentailData[fields.inputName]} onChange={(t)=>setCredentialData(prev=>({...prev,[fields.inputName]:t.target.value}))} id={fields.linkingName}>
+      //   <option value=''>SELECT</option>
+      //   {fields.option?.map((el,index)=>{
+      //     return(
+      //       <>
+      //       <option value={el} key={index}>{el}</option>
+      //       </>
+      //     )
+      //   })}
+      // </select>
+      <Select  value={CredentailData[fields.inputName]} onValueChange={(v)=>setCredentialData(prev=>({...prev,[fields.inputName]:v}))}>
+              <SelectTrigger id="gender" className=" border-theme-blue  outline-none h-9    bg-white">
+                <SelectValue placeholder={fields.linkingName} />
+              </SelectTrigger>
+              <SelectContent>
+         
+            {fields.option?.map((el,index)=>{
+              return  <SelectItem key={index} value={el}>{el}</SelectItem>
+            })}
+              </SelectContent>
+            </Select>
       }
       
           <p className="text-white   text-xs mt-2   ">{Warning[fields.inputName]?Warning[fields.inputName]:''}</p>
